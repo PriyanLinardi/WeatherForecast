e13c24f9d2e18ecffe8eb9a038863c04f229f94c
PriyanLinardi <96510386+PriyanLinardi@users.noreply.github.com>
2021-12-24 00:26:09 +0700

    Add files via upload

---
+++ WeatherForecast/WeatherInformation.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WeatherInformation.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WeatherInformation.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("25c420d0-a155-45e8-989b-41c70c34a743")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WeatherForecast/WeatherInformation.Test/WeatherTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WeatherForecast.Controllers;
+
+namespace WeatherInformation.Test
+{
+    [TestClass]
+    public class WeatherTest
+    {
+        public string countryCode = "INA";
+        public string cityName = "Jakarta";
+
+        [TestMethod]
+        public void IsCountryAndCityValid_CountryCodeAndCityCorrect_ReturnsTrue()
+        {
+            // Arrange
+            var weather = new WeatherController();
+
+            // Act
+            var result = weather.IsCountryAndCityValid(countryCode, cityName);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void GetWeatherInformation_CallAPIAndConvertJsonCorrect_ReturnsTrue()
+        {
+            // Arrange
+            var weather = new WeatherController();
+
+            // Act
+            bool result = true;
+            var weatherInfo = weather.GetWeatherInformation(countryCode, cityName);
+            if (weatherInfo.Contains("Error"))
+                result = false;
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+    }
+}
